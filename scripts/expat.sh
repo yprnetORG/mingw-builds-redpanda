133b9976705dc653d574d78afc480287dc49d9fb
yprnet <yuanpeirong@qq.com>
2024-12-16 16:54:53 +0800

    expat updated to 2.6.4

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.6.2
+PKG_VERSION=2.6.4
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.xz
